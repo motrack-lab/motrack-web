1574f68349f8046f990b3cc72e9481bca61b81a1
RAFIF-RF10 <rafifgithub24@gmail.com>
2026-06-21 11:49:36 +0700

    feat: add CafePage component with navigation, pricing, and footer structure

--- src/pages/CafePage/index.jsx
+++ src/pages/CafePage/index.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../../components/common/Navbar";
 import Footer from "../../components/common/Footer";
 import CafePricing from "./CafePricing";
 
 const CafePage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="bg-white pt-24">
       <Navbar />
